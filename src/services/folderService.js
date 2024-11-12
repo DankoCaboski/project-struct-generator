5c7edb5b536e734d4767dd5e2d153c9338b9488a
Danko Caboski <caboskidanko@gmail.com>
2024-11-12 16:08:55 -0300

    Feat: base do import

--- src/services/folderService.js
+++ src/services/folderService.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+const path = require('path');
 
 function folderExists(projctRootPath) {
 
@@ -12,11 +13,24 @@ function folderExists(projctRootPath) {
 function createFolder(projctRootPath) {
   try {
     fs.mkdirSync(projctRootPath);
+    if (projctRootPath.includes("10 - Relatorios e Manuais")) {
+      importDocs(projctRootPath); 
+    }
     return true;
   } catch (err) {
     console.error(err);
     return false;
   }
+}
+
+function importDocs(projctRootPath){
+  const docsPath = "\\\\AXIS-SERVER\\02_Projetos\\00_Axis\\#Documentos modelos";
+  const docs = fs.readdirSync(docsPath);
+  docs.forEach(doc => {
+    const docPath = path.join(docsPath, doc);
+    const destPath = path.join(projctRootPath, doc);
+    fs.copyFileSync(docPath, destPath);
+  });
 }
 
 module.exports = {
